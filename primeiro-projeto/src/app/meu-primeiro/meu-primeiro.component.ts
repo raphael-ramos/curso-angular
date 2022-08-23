7d122164b38b1632343d7511228efaad0d6559ea
Raphael Ramos <raphael.ramos95@hotmail.com>
2022-08-23 20:03:47 -0300

    Criado primeiro projeto e primeiros componentes

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',
+    template:`
+        <p>Meu primeiro component com Angular</p>
+    `
+})
+
+export class MeuPrimeiroComponent {
+
+}
