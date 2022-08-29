45f8ef84eb45666b922df664cfb13167e5fc8258
Raphael Ramos <raphael.ramos95@hotmail.com>
2022-08-29 19:41:50 -0300

    Introdução ao typescript

---
+++ hello-typescript/main.ts
@@ -0,0 +1,19 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x,y){
+    return x +y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+
+numeros.map(function(valor){
+    return valor * 2
+});
+
+numeros.map( valor => valor * 2);
+
+var n1: any = 'djkahskhdjkash';
+n1 = 4
