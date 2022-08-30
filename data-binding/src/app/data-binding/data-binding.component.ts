3aecc660deb6b66a21409710be59a5167b5cc354
Raphael Ramos <raphael.ramos95@hotmail.com>
2022-08-30 19:57:39 -0300

    Aprendendo sobre event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -12,12 +12,34 @@ export class DataBindingComponent implements OnInit {
 
   cursoAngular = true;
 
+  valorAtual:string='';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true
+  }
+
+  botaoClicado(){
+    alert('clicado!')
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    console.log(event)
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
